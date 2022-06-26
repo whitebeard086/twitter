4449542dcbae6dfc34e315fef90e12044f1dbf44
The Exorcist <whitebeard086@gmail.com>
2022-06-26 23:15:27 +0100

    implement like post functionality

--- components/Post.jsx
+++ components/Post.jsx
@@ -6,9 +6,55 @@ import {
   ShareIcon,
   TrashIcon,
 } from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc,
+} from "firebase/firestore";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import Moment from "react-moment";
+import { db } from "../firebase";
 
 const Post = ({ post }) => {
+  const [liked, setLiked] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", post.id, "likes"),
+      (snapshot) => {
+        setLiked(snapshot.docs);
+      }
+    );
+  }, [post.id]);
+
+  useEffect(() => {
+    setHasLiked(
+      liked.findIndex((like) => like.id === session?.user.uid) !== -1
+    );
+  }, [liked, session?.user.uid]);
+
+  const likePost = async () => {
+    if (session) {
+      if (hasLiked) {
+        await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid));
+      } else {
+        await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid), {
+          username: session.user.username,
+        });
+      }
+    } else {
+      alert("You must be logged in to like a post");
+      signIn();
+    }
+  };
+
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
       {/* user image */}
@@ -32,9 +78,7 @@ const Post = ({ post }) => {
               @{post.data().username} -{" "}
             </span>
             <span className="text-sm sm:text-[15px] hover:underline">
-              <Moment fromNow>
-                {post.data().timestamp.toDate()}
-              </Moment>
+              <Moment fromNow>{post.data().timestamp?.toDate()}</Moment>
             </span>
           </div>
 
@@ -60,7 +104,24 @@ const Post = ({ post }) => {
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon className="h-7 w-7 hover-effect hover:text-sky-500 hover:bg-sky-100" />
           <TrashIcon className="h-7 w-7 hover-effect hover:text-red-600 hover:bg-red-100" />
-          <HeartIcon className="h-7 w-7 hover-effect hover:text-red-600 hover:bg-red-100" />
+          <div className="flex items-center">
+            {hasLiked ? (
+              <HeartIconFilled
+                onClick={likePost}
+                className="h-7 w-7 hover-effect text-red-600 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                onClick={likePost}
+                className="h-7 w-7 hover-effect hover:text-red-600 hover:bg-red-100"
+              />
+            )}
+            {liked.length > 0 && (
+              <span className={`${hasLiked && "text-red-600"} text-sm ml-1`}>
+                {liked.length}
+              </span>
+            )}
+          </div>
           <ShareIcon className="h-7 w-7 hover-effect hover:text-sky-500 hover:bg-sky-100" />
           <ChartBarIcon className="h-7 w-7 hover-effect hover:text-sky-500 hover:bg-sky-100" />
         </div>
