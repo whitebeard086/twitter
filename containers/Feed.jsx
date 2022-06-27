03f49fec32d866a76ac2d94d9d7af8aafc9b31da
The Exorcist <whitebeard086@gmail.com>
2022-06-27 05:51:22 +0100

    animation effect with framer motion

--- containers/Feed.jsx
+++ containers/Feed.jsx
@@ -1,5 +1,6 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Input, Post } from "../components";
 import { db } from "../firebase";
@@ -28,9 +29,19 @@ const Feed = () => {
       {/* Input field */}
       <Input />
       {/* Posts feed */}
-      {posts.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      <AnimatePresence>
+        {posts.map((post) => (
+          <motion.div
+            key={post.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <Post post={post} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
--- containers/Widgets.jsx
+++ containers/Widgets.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 import { SearchIcon } from "@heroicons/react/outline";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { News } from "../components";
 
@@ -23,9 +24,19 @@ const Widgets = ({ newsResults, randomUsers }) => {
 
       <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">What&apos;s happening</h4>
-        {newsResults.slice(0, articleNum).map((article) => (
-          <News key={article.title} article={article} />
-        ))}
+        <AnimatePresence>
+          {newsResults.slice(0, articleNum).map((article) => (
+            <motion.div
+              key={article.title}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <News article={article} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
         <button
           onClick={() => setArticleNum(articleNum + 3)}
           className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
@@ -36,31 +47,42 @@ const Widgets = ({ newsResults, randomUsers }) => {
 
       <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">Who to follow</h4>
-        {randomUsers.slice(0, userNum).map((user) => (
-          <div
-            className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200 transition duration-200"
-            key={user.login.username}
-          >
-            <img
-              className="rounded-full"
-              width="40"
-              src={user.picture.thumbnail}
-              alt="user-image"
-            />
-            <div className="truncate ml-4 leading-5">
-              <h4 className="font-bold hover:underline text-[14px] truncate">
-                {user.login.username}
-              </h4>
-              <h5 className="text-[13px] text-gray-500 truncate">
-                {user.name.first + " " + user.name.last}
-              </h5>
-            </div>
-            <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">
-              Follow
-            </button>
-          </div>
-        ))}
-        <button onClick={() => setUserNum(userNum + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
+        <AnimatePresence>
+          {randomUsers.slice(0, userNum).map((user) => (
+            <motion.div
+              key={user.login.username}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <div className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200 transition duration-500 ease-out">
+                <img
+                  className="rounded-full"
+                  width="40"
+                  src={user.picture.thumbnail}
+                  alt="user-image"
+                />
+                <div className="truncate ml-4 leading-5">
+                  <h4 className="font-bold hover:underline text-[14px] truncate">
+                    {user.login.username}
+                  </h4>
+                  <h5 className="text-[13px] text-gray-500 truncate">
+                    {user.name.first + " " + user.name.last}
+                  </h5>
+                </div>
+                <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">
+                  Follow
+                </button>
+              </div>
+            </motion.div>
+          ))}
+        </AnimatePresence>
+
+        <button
+          onClick={() => setUserNum(userNum + 3)}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+        >
           Show more
         </button>
       </div>
